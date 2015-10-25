95016892bc841f8c5eebff3ef5a28727e3f01db1
Lukas Gräßle <Lukas Grle>
2015-10-25 16:44:43 +0100

    close #1 remove AngularJS dependencies

---
+++ scripts/model-editor/editor.js
@@ -0,0 +1,6 @@
+
+var svg = d3.selectAll('svg');
+var circle = svg.selectAll('circle');
+
+circle.style("fill", "steelblue");
+circle.attr("r", 30);
--- scripts/model-editor/module.js
+++
@@ -1,1 +0,0 @@
-angular.module('de.devjs.modelEditor', []);
